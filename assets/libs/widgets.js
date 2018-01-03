64ff19b016288bb82d09f8697d86d25f380f237f
Xmal Morthen <askmeyou15@hotmail.com>
2018-01-03 08:05:37 -0600

    Signed-off-by: Xmal Morthen <askmeyou15@hotmail.com>

--- assets/libs/widgets.js
+++ assets/libs/widgets.js
@@ -1,11 +1,11 @@
-var bookVisitFlexWidgetScriptLoaded = false;
-function bookVisitInitJQuery() {
+var widgetHMHotel = false;
+function widgetHMHotelInitJQuery() {
 	if (typeof (bvJQuery) == "undefined") {
-		if (!bookVisitFlexWidgetScriptLoaded) {
-			bookVisitFlexWidgetScriptLoaded = true;
+		if (!widgetHMHotel) {
+			widgetHMHotel = true;
 			document.write("<scr" + "ipt charset=\"UTF-8\" type=\"text/javascript\" src=\"//online.bookvisit.com/Content/flexwidget/FlexWidgetFullScripts.min.js\"></scr" + "ipt>");
 		}
-		setTimeout("bookVisitInitJQuery()", 50);
+		setTimeout("widgetHMHotelInitJQuery()", 50);
 	}
 	else {
 		var baseUrl = "//online.bookvisit.com/v2/widget/getwidget";
@@ -16,9 +16,81 @@ function bookVisitInitJQuery() {
 					function (json) {
 						$("#widgetHMHotel").html(json.Widget);
 					}
+				);                                
+			}
+		)(bvJQuery);
+	}
+}
+widgetHMHotelInitJQuery();
+
+var widgetMontroi_City = false;
+function widgetMontroi_CityInitJQuery() {
+	if (typeof (bvJQuery) == "undefined") {
+		if (!widgetMontroi_City) {
+			widgetMontroi_City = true;
+			document.write("<scr" + "ipt charset=\"UTF-8\" type=\"text/javascript\" src=\"//online.bookvisit.com/Content/flexwidget/FlexWidgetFullScripts.min.js\"></scr" + "ipt>");
+		}
+		setTimeout("widgetMontroi_CityInitJQuery()", 50);
+	}else {
+		var baseUrl = "//online.bookvisit.com/v2/widget/getwidget";
+		(
+			function ($) {
+				$.getJSON(
+					baseUrl + "?channelId=2bc08c22-5ec2-4440-92db-8caed7906647&culture=es-ES&containerId=widgetMontroi_City&layout=Standard1Column&openOption=redirect&displayPromoCode=false&displayCorpCode=false&displayIATACode=false&jsoncallback=?",
+					function (json) {
+						$("#widgetMontroi_City").html(json.Widget);
+					}
 				);
 			}
 		)(bvJQuery);
 	}
 }
-bookVisitInitJQuery();
+widgetMontroi_CityInitJQuery();
+
+var widgetEL_HOTELITO = false;
+function widgetEL_HOTELITOInitJQuery() {
+	if (typeof (bvJQuery) == "undefined") {
+		if (!widgetEL_HOTELITO) {
+			widgetEL_HOTELITO = true;			
+                        document.write("<scr" + "ipt charset=\"UTF-8\" type=\"text/javascript\" src=\"//online.bookvisit.com/Content/flexwidget/FlexWidgetFullScripts.min.js\"></scr" + "ipt>");
+		}
+		setTimeout("widgetEL_HOTELITOInitJQuery()", 50);
+	}else {
+		var baseUrl = "//online.bookvisit.com/v2/widget/getwidget";
+		(
+			function ($) {
+                                $.getJSON(
+					baseUrl + "?channelId=0a2c95bd-3202-4493-b24c-fbeca7d1bcda&culture=es-ES&containerId=widgetEL_HOTELITO&layout=Standard1Column&openOption=redirect&displayPromoCode=false&displayCorpCode=false&displayIATACode=false&jsoncallback=?",
+                            function (json) {
+						$("#widgetEL_HOTELITO").html(json.Widget);
+					}
+				);
+			}
+		)(bvJQuery);
+	}
+}
+widgetEL_HOTELITOInitJQuery();
+
+var widgetMontroi_Express = false;
+function widgetMontroi_ExpressInitJQuery() {
+	if (typeof (bvJQuery) == "undefined") {
+		if (!widgetMontroi_Express) {
+			widgetMontroi_Express = true;			
+                        document.write("<scr" + "ipt charset=\"UTF-8\" type=\"text/javascript\" src=\"//online.bookvisit.com/Content/flexwidget/FlexWidgetFullScripts.min.js\"></scr" + "ipt>");
+		}
+		setTimeout("widgetMontroi_ExpressInitJQuery()", 50);
+	}else {
+		var baseUrl = "//online.bookvisit.com/v2/widget/getwidget";
+		(
+			function ($) {
+                                $.getJSON(
+					baseUrl + "?channelId=b885cf9c-ab87-46b6-8a77-a9e4dde24303&culture=es-ES&containerId=widgetMontroi_Express&layout=Standard1Column&openOption=redirect&displayPromoCode=false&displayCorpCode=false&displayIATACode=false&jsoncallback=?",
+                            function (json) {
+						$("#widgetMontroi_Express").html(json.Widget);
+					}
+				);
+			}
+		)(bvJQuery);
+	}
+}
+widgetMontroi_ExpressInitJQuery();
